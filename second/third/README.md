ade16a62b4bf0551fb374b2129b45bb92d348b27
jsqiao <747848783@qq.com>
2017-10-24 19:32:42 +0800

    重建二叉树

--- second/third/README.md
+++ second/third/README.md
@@ -1,8 +1,9 @@
 # 数据结构
 
 ## 面试题3 二维数组中的查找
-### 题目：二维数组中，每行从左到右递增，每列从上到下递增，给出一个数，判断它是否在数组中
-### 思路：从左下角或者右上角开始比较
+> 题目：二维数组中，每行从左到右递增，每列从上到下递增，给出一个数，判断它是否在数组中
+>
+> 思路：从左下角或者右上角开始比较
 
 ```python
 def find_integer(matrix, num):
@@ -26,12 +27,15 @@ def find_integer(matrix, num):
 ```
 
 ## 面试题4 替换空格
-### 题目：把字符串中的空格替换成'20%'
-###  方法1：直接使用Python字符串的内置函数
+> 题目：把字符串中的空格替换成'20%'
+>
+>方法1：直接使用Python字符串的内置函数
+
 ```python
 >>> ' a  b  '.replace(' ', '20%')
 ```
-### 方法2: 使用正则表达式
+> 方法2: 使用正则表达式
+
 ```python
 >>> import re
 >>> ret = re.compile(' ')
@@ -39,7 +43,8 @@ def find_integer(matrix, num):
 ```
 
 ## 面试题5 从尾到头打印单链表
-### 方法1：使用栈,可以使用列表模拟
+> 方法1：使用栈,可以使用列表模拟
+
 ```python
 def print_links(links):
     stack = []
@@ -49,7 +54,8 @@ def print_links(links):
     while stack:
         print stack.pop()
 ```
-### 方法2：直接递归
+> 方法2：直接递归
+
 ```python
 def print_link_recursion(links):
     if links:
@@ -58,5 +64,25 @@ def print_link_recursion(links):
 ```
 
 ## 面试题6 重建二叉树
-### 要求：用前序和中序遍历结果构建二叉树，遍历结果中不包含重复值
-### 思路：前序的第一个元素是根结点的值，在中序中找到该值，中序中该值的左边的元素是根结点的左子树，右边是右子树，然后递归的处理左边和右边
+> 要求：用前序和中序遍历结果构建二叉树，遍历结果中不包含重复值
+>
+> 思路：前序的第一个元素是根结点的值，在中序中找到该值，中序中该值的左边的元素是根结点的左子树，右边是右子树，然后递归的处理左边和右边
+>
+> **提示**：二叉树结点，以及对二叉树的各种操作，测试代码见six.py
+```python
+def construct_tree(preorder=None, inorder=None):
+    """
+    构建二叉树
+    """
+    if not preorder or not inorder:
+        return None
+    index = inorder.index(preorder[0])
+    left = inorder[0:index]
+    right = inorder[index+1:]
+    root = TreeNode(preorder[0])
+    root.left = construct_tree(preorder[1:1+len(left)], left)
+    root.right = construct_tree(preorder[-len(right):], right)
+    return root
+```
+
+## 面试题7 用两个栈实现队列
---
+++ second/third/six.py
@@ -0,0 +1,100 @@
+# coding=utf-8
+
+"""
+使用先序遍历和中序遍历的结果重建二叉树
+"""
+from collections import deque
+
+
+class TreeNode(object):
+    """
+    二叉树结点定义
+    """
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Tree(object):
+    """
+    二叉树
+    """
+    def __init__(self):
+        self.root = None
+
+    def bfs(self):
+        ret = []
+        queue = deque([self.root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                ret.append(node.val)
+                queue.append(node.left)
+                queue.append(node.right)
+        return ret
+
+    def pre_traversal(self):
+        ret = []
+
+        def traversal(head):
+            if not head:
+                return
+            ret.append(head.val)
+            traversal(head.left)
+            traversal(head.right)
+
+        traversal(self.root)
+        return ret
+
+    def in_traversal(self):
+        ret = []
+
+        def traversal(head):
+            if not head:
+                return
+            traversal(head.left)
+            ret.append(head.val)
+            traversal(head.right)
+
+        traversal(self.root)
+        return ret
+
+    def post_traversal(self):
+        ret = []
+
+        def traversal(head):
+            if not head:
+                return
+            traversal(head.left)
+            traversal(head.right)
+            ret.append(head.val)
+
+        traversal(self.root)
+        return ret
+
+
+def construct_tree(preorder=None, inorder=None):
+    """
+    构建二叉树
+    """
+    if not preorder or not inorder:
+        return None
+    index = inorder.index(preorder[0])
+    left = inorder[0:index]
+    right = inorder[index+1:]
+    root = TreeNode(preorder[0])
+    root.left = construct_tree(preorder[1:1+len(left)], left)
+    root.right = construct_tree(preorder[-len(right):], right)
+    return root
+
+
+if __name__ == '__main__':
+    t = Tree()
+    root = construct_tree([1, 2, 4, 7, 3, 5, 6, 8], [4, 7, 2, 1, 5, 3, 8, 6])
+    t.root = root
+    print t.bfs()
+    print t.pre_traversal()
+    print t.in_traversal()
+    print t.post_traversal()
+
